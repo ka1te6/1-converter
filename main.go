4d13bf29f0ac37824b3fe51100be196020882b1a
ka1te6 <a.andrey2006@yandex.ru>
2025-07-06 02:22:09 +0300

    дз 2

--- main.go
+++ main.go
@@ -1,10 +1,23 @@
 package main
 
-import "fmt"
+import (
+	"fmt"
+)
 
 func main() {
 	const UsdToEur = 0.85
 	const UsdToRub = 78.6
 	EurToRub := UsdToRub / UsdToEur
 	fmt.Print(EurToRub)
+}
+func getUserInput() (string, string) {
+	var orgValue string
+	var tarValue string
+	fmt.Scan(&orgValue)
+	fmt.Scan(&tarValue)
+	return orgValue, tarValue
+}
+
+func convertate(orgValue string, tarValue string, sum int) int {
+	return 0
 }
